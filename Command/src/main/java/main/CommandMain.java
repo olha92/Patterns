a844ada7588fc15a8657bfedf3d630c819ee1e54
olga <elza@post.ua>
2016-10-16 14:45:29 +0300

    [command] initial

---
+++ Command/src/main/java/main/CommandMain.java
@@ -0,0 +1,14 @@
+package main;
+
+import org.apache.log4j.Logger;
+
+/**
+ * CommandMain for example
+ */
+public class CommandMain {
+    private static final Logger log = Logger.getLogger(CommandMain.class);
+
+    public static void main(String[] args) {
+        log.info("Start method main");
+    }
+}
---
+++ Command/src/main/java/pattern/behavior/command/Command.java
@@ -0,0 +1,4 @@
+package pattern.behavior.command;
+
+public abstract class Command {
+}
